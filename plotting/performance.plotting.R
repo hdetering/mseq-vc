1634effae75aa0b8af4b1a1faa4a2952bfee8c63
lauratl <lauratomaslopez@gmail.com>
2020-08-02 14:31:29 +0200

    Removed MuClone_perf

--- plotting/performance.plotting.R
+++ plotting/performance.plotting.R
@@ -136,6 +136,7 @@ plot_perf_min_mean <- function ( df )
 plot_perf_freq <- function ( df )
 {
   # define order of variant callers (will affect plots)
+  df = df %>% filter(caller!= "MuClone_perf")
   df$caller = factor(df$caller, levels = c(
     'Bcftools', 
     'CaVEMan', 
